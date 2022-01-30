b837f967cce5c9606823f2c0980a6abf02d62e19
Francesco Forte <francesco.forte831tech@gmail.com>
2022-01-30 00:07:30 -0500

    more visually complete login and sign up pages

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,12 @@
+from xmlrpc.client import boolean
 from flask import Blueprint, render_template
+from sqlalchemy import true
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template("login.html", text="Testing", user="Tim")
+    return render_template("login.html", boolean = True)
 
 @auth.route('/logout')
 def logout():
@@ -13,4 +15,8 @@ def logout():
 @auth.route('/sign-up')
 def sign_up():
     return render_template("sign_up.html")
+
+@auth.route('/secret-page')
+def secret_page():
+    return "<p>You found the secret page</p>"
 
