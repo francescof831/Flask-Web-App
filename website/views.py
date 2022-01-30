226926a1f7e4f548d3c9c188fddd5dcbcf0bfced
Francesco Forte <francesco.forte831tech@gmail.com>
2022-01-29 23:16:35 -0500

    second commit

--- website/views.py
+++ website/views.py
@@ -1,10 +1,10 @@
 #all the pages not related to authentication that user can nav to are here
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
 
     
